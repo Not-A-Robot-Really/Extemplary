1b690c258d59b304d2b4574df56dfe9ff98c1cf1
Lantern834 <ziruiwang662@gmail.com>
2026-07-30 14:17:13 -0400

    Renamed indexicons, landing app, and landingicons to match what the index html was asking for

--- index-icons.js
+++ index-icons.js
@@ -2,4 +2,4 @@
 // into #iconSpriteMount so all existing <use href="#icon-N"> references keep working.
 fetch('icons.svg').then(r => r.text()).then(svgText => {
   document.getElementById('iconSpriteMount').innerHTML = svgText;
-});
+})
